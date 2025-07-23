c642f1e3be9ce153b60cb92a39e34543a7e06308
David Bittencourt <muitocomplicado@gmail.com>
2025-07-23 08:36:55 -0300

    Open browser when starting local server

--- server.sh
+++ server.sh
@@ -8,5 +8,5 @@ echo "Opening http://localhost:3000"
 echo "Press Ctrl+C to stop the server"
 echo ""
 
-# Start Python's built-in HTTP server
+open "http://localhost:3000/"
 python3 -m http.server 3000
